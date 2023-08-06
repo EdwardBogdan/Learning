f496c11e07efe086bb8a83b9bfcbae97e6aa8a18
EdwardBogdan <Edoorman75@gmail.com>
2023-08-06 19:54:42 +0200

    CourseWork

---
+++ My project/Assets/Scripts/Components/AwakeComponent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MyProject.Components
+{
+    public class AwakeComponent : MonoBehaviour
+    {
+        [SerializeField] UnityEvent _action;
+
+        private void Awake()
+        {
+            _action?.Invoke();
+        }
+    }
+}
